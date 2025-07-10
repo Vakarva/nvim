f15aee715418977ff91b7f5e8e039a99605b6098
Vakarva <151787968+Vakarva@users.noreply.github.com>
2025-07-10 00:06:08 -0700

    Adjust keymappings for Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,15 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-        { '<leader>fd', '<cmd>Telescope find_files<cr>', desc = 'Telescope find files' },
-        { '<C-f>', '<cmd>Telescope git_files<cr>', desc = 'Telescope find git files' },
-        { '<leader>fg', function()
-            require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') })
-        end, desc = 'Telescope project search' }
-    }
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	keys = {
+		{ '<leader>fd', '<cmd>Telescope find_files<cr>', desc = 'Telescope find files' },
+		{ '<C-f>', '<cmd>Telescope git_files<cr>', desc = 'Telescope find git files' },
+		{ '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Telescope live grep search' },
+		{ '<leader>fw', '<cmd>Telescope grep_string<cr>', desc = 'Telescope grep word under cursor' },
+		{ '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Telescope buffers' },
+		{ '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Telescope help tags' },
+		{ '<leader>fr', '<cmd>Telescope oldfiles<cr>', desc = 'Telescope recent files' },
+		{ '<leader>fc', '<cmd>Telescope colorscheme<cr>', desc = 'Telescope colorschemes' },
+	},
 }
