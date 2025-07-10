125df5409c7f003d7608354f346cf8935a8e64a9
Vakarva <151787968+Vakarva@users.noreply.github.com>
2025-07-10 00:52:13 -0700

    Add filepath and file modified to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,19 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-        options = {
-            theme = 'dracula'
-        }
-    },
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	opts = {
+		options = {
+			theme = 'dracula',
+		},
+		sections = {
+			lualine_c = {
+				{
+					'filename',
+					file_status = true,
+					path = 1,
+					shorting_target = 50,
+				},
+			},
+		},
+	},
 }
