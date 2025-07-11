b6f99844133838b7a238d7bce7b04d48434c3c46
Vakarva <151787968+Vakarva@users.noreply.github.com>
2025-07-11 10:20:34 -0700

    Integrate oil-git-status
    Also update icons to nvim-web-devicons

---
+++ lua/plugins/oil-git-status.lua
@@ -0,0 +1,7 @@
+return {
+	'refractalize/oil-git-status.nvim',
+	config = true,
+	dependencies = {
+		'stevearc/oil.nvim',
+	},
+}
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,22 +1,30 @@
 return {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpt
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    keys = {
-        { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
-        { '<leader>-', function() require('oil').toggle_float() end,
-        desc = 'Toggle oil floating window' },
-    },
-    -- Lazy loading not recommended because very tricky to make it work correctly in all situations
-    lazy = false,
-    opts = {
-        view_options = {
-            show_hidden = true,
-            is_always_hidden = function(name, _)
-                local folder_skip = { '.git' }
-                return vim.tbl_contains(folder_skip, name)
-            end,
-        }
-    },
+	'stevearc/oil.nvim',
+	---@module 'oil'
+	---@type oil.SetupOpt
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	keys = {
+		{ '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
+		{
+			'<leader>-',
+			function()
+				require('oil').toggle_float()
+			end,
+			desc = 'Toggle oil floating window',
+		},
+	},
+	-- Lazy loading not recommended because very tricky to make it work correctly in all situations
+	lazy = false,
+	opts = {
+		view_options = {
+			show_hidden = true,
+			is_always_hidden = function(name, _)
+				local folder_skip = { '.git' }
+				return vim.tbl_contains(folder_skip, name)
+			end,
+		},
+		win_options = {
+			signcolumn = 'yes:2',
+		},
+	},
 }
