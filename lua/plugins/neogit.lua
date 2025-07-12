73aebaf72ced56ca8de23c2d4ee1eb2930d3cfaf
Vakarva <151787968+Vakarva@users.noreply.github.com>
2025-07-12 13:31:19 -0700

    Format

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,12 +1,12 @@
 return {
-    'NeogitOrg/neogit',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'sindrets/diffview.nvim', -- optional but recommended
-        'nvim-telescope/telescope.nvim', -- optional
-    },
-    config = true,
-    keys = {
-        { '<leader>gs', '<cmd>Neogit<cr>', desc = 'Neogit Status' }
-    }
+	'NeogitOrg/neogit',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'sindrets/diffview.nvim',
+		'nvim-telescope/telescope.nvim',
+	},
+	config = true,
+	keys = {
+		{ '<leader>gs', '<cmd>Neogit<cr>', desc = 'Neogit Status' },
+	},
 }
