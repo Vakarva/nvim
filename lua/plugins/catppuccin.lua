e2297bafd46c068c42a221dbbdaeeffc9f76d1bd
Vakarva <151787968+Vakarva@users.noreply.github.com>
2025-07-31 23:03:51 -0700

    Update Catppuccin integration, Add Tokyonight
    Catppuccin: Use auto integrations and add undercurl for errors.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,11 +1,27 @@
 return {
-    {
-        'catppuccin/nvim',
-        lazy = false,
-        name = 'catppuccin',
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme 'catppuccin-mocha'
-        end,
-    },
+	{
+		'catppuccin/nvim',
+		lazy = false,
+		name = 'catppuccin',
+		opts = {
+			auto_integrations = true,
+			flavour = 'mocha',
+			integrations = {
+				native_lsp = {
+					enabled = true,
+					underlines = {
+						errors = { 'undercurl' },
+						hints = { 'undercurl' },
+						warnings = { 'undercurl' },
+						information = { 'undercurl' },
+					},
+				},
+			},
+		},
+		priority = 1000,
+		config = function(_, opts)
+			require('catppuccin').setup(opts)
+			vim.cmd.colorscheme('catppuccin')
+		end,
+	},
 }
