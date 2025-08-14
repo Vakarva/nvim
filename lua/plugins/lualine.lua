e0acbc63a7fce20cbf88549ab3fdc283b335cb7d
Vakarva <151787968+Vakarva@users.noreply.github.com>
2025-08-13 21:55:38 -0700

    Update lualine to use slash separators
    More simple.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,6 +3,8 @@ return {
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	opts = {
 		options = {
+			component_separators = { left = '', right = '' },
+			section_separators = { left = '', right = '' },
 			theme = 'catppuccin',
 		},
 		sections = {
