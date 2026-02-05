636162bb1bf8232aa5924e5b6b66ba0c3cdeec44
Vakarva <151787968+Vakarva@users.noreply.github.com>
2026-02-05 10:06:10 -0800

    feat(kanagawa): remove color customization

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,14 +1,6 @@
 return {
     'rebelot/kanagawa.nvim',
     config = function()
-        require('kanagawa').setup({
-            colors = {
-                palette = {
-                    dragonBlack0 = '#000000',
-                    dragonBlack3 = '#151210',
-                },
-            },
-        })
         vim.cmd.colorscheme('kanagawa-dragon')
     end,
     lazy = false,
