639c00992f12381613a42f3df4ffb1b389514a64
Vakarva <151787968+Vakarva@users.noreply.github.com>
2026-07-02 14:58:36 -0700

    refactor(harpoon): defer loading and restore cursor on select
    Move keys to a module-level table with the requires deferred to keypress
    time, remap prev/next to <leader>hp / <leader>hn, and restore the saved
    cursor position when selecting a file.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,75 +1,62 @@
+local keys = {
+    {
+        '<leader>ha',
+        function()
+            require('harpoon'):list():add()
+        end,
+        desc = 'Harpoon add file',
+    },
+    {
+        '<C-e>',
+        function()
+            local harpoon = require('harpoon')
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = 'Harpoon quick menu',
+    },
+    {
+        '<leader>hp',
+        function()
+            require('harpoon'):list():prev()
+        end,
+        desc = 'Harpoon prev',
+    },
+    {
+        '<leader>hn',
+        function()
+            require('harpoon'):list():next()
+        end,
+        desc = 'Harpoon next',
+    },
+}
+
+for i = 1, 4 do
+    table.insert(keys, {
+        '<leader>' .. i,
+        function()
+            require('harpoon'):list():select(i)
+        end,
+        desc = 'Harpoon to file ' .. i,
+    })
+end
+
 return {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = function()
-        local harpoon = require('harpoon')
-        return {
-            {
-                '<leader>ha',
-                function()
-                    harpoon:list():add()
-                end,
-                desc = 'Harpoon add file',
-            },
-            {
-                '<C-e>',
-                function()
-                    harpoon.ui:toggle_quick_menu(harpoon:list())
-                end,
-                desc = 'Harpoon quick menu',
-            },
-            {
-                '<leader>1',
-                function()
-                    harpoon:list():select(1)
-                end,
-                desc = 'Harpoon to file 1',
-            },
-            {
-                '<leader>2',
-                function()
-                    harpoon:list():select(2)
-                end,
-                desc = 'Harpoon to file 2',
-            },
-            {
-                '<leader>3',
-                function()
-                    harpoon:list():select(3)
-                end,
-                desc = 'Harpoon to file 3',
-            },
-            {
-                '<leader>4',
-                function()
-                    harpoon:list():select(4)
-                end,
-                desc = 'Harpoon to file 4',
-            },
-            {
-                '[h',
-                function()
-                    harpoon:list():prev()
-                end,
-                desc = 'Harpoon prev',
-            },
-            {
-                ']h',
-                function()
-                    harpoon:list():next()
-                end,
-                desc = 'Harpoon next',
-            },
-        }
-    end,
+    keys = keys,
     opts = {
         default = {
+            -- Like the default select, but escapes special characters in the path
             select = function(list_item)
                 if list_item == nil then
                     return
                 end
                 vim.cmd.edit(vim.fn.fnameescape(list_item.value))
+                local ctx = list_item.context
+                if ctx and ctx.row then
+                    pcall(vim.api.nvim_win_set_cursor, 0, { ctx.row, ctx.col or 0 })
+                end
             end,
         },
     },
